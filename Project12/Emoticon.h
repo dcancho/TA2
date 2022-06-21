4f1b4a6cdbedf5e571bf93e1f287d0fe1f179c68
Jose Anthony Arenas Conde <u20211d744@upc.edu.pe>
2022-06-21 12:18:57 -0500

    add

---
+++ Project12/Emoticon.h
@@ -0,0 +1,27 @@
+#pragma once
+#include"Boca.h"
+#include"Figure.h"
+#include"Nariz.h"
+#include"Ojos.h"
+
+class Emoticon {
+public:
+	Emoticon() {
+
+	}
+	~Emoticon(){}
+
+	void CaraTriste{
+
+
+
+	}
+	void CaraAsombrada{
+
+	}
+	void CaraSeria{
+
+
+	}
+
+};
