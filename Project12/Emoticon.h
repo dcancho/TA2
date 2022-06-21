14d1b13724a639952176778bc3a8d5463118c80e
Jose Anthony Arenas Conde <u20211d744@upc.edu.pe>
2022-06-21 12:31:19 -0500

    nariz

--- Project12/Emoticon.h
+++ Project12/Emoticon.h
@@ -7,12 +7,14 @@
 class Emoticon {
 private:
 	Ojos ojo1;
-	ojo1 = new ojo(30, 10, 0, 0, 0);
 	Ojos ojo2;
-	ojo2 = new ojo(50, 10, 0, 0, 0);
-public:
-	Emoticon() {
+	Nariz nariz;
 
+public:
+	Emoticon(){
+		ojo1 = new Ojos(30, 10, 0, 0, 0);
+		ojo2 = new Ojos(50, 10, 0, 0, 0);
+		nariz = new Nariz(40,25,0,0,0);
 	}
 	~Emoticon(){}
 
